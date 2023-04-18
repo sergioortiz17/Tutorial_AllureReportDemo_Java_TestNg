1432ab2458e8f7ca92d5d0ff7625316931ca70ef
sergioo2 <sergioo@epidataconsulting.com>
2023-04-18 18:25:02 -0300

    first commit open report allure with Java and TestNg success

---
+++ src/test/java/com/sdetadda/allure/Testcase1.java
@@ -0,0 +1,17 @@
+package com.sdetadda.allure;
+
+import org.testng.annotations.Test;
+import org.testng.AssertJUnit;
+import org.testng.annotations.Test;
+import org.testng.AssertJUnit;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Testcase1 {
+  @Test
+  public void testMethod1() {
+	  AssertJUnit.assertTrue(false);
+	  System.out.println("testMethod1 is not working as expected");
+	  
+  }
+}
---
+++ src/test/java/com/sdetadda/allure/Testcase2.java
@@ -0,0 +1,17 @@
+package com.sdetadda.allure;
+
+import org.testng.annotations.Test;
+import org.testng.AssertJUnit;
+import org.testng.annotations.Test;
+import org.testng.AssertJUnit;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Testcase2 {
+  @Test
+  public void testMethod2() {
+	  AssertJUnit.assertTrue(true);
+	  System.out.println("testMethod2 is working as expected");
+	  
+  }
+}
